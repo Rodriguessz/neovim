f6f14b6335fe6bb1524d99dfd057a7076d46be8f
Enzo Rodrigues <rodrigues@Enzos-MacBook-Pro.local>
2025-04-06 17:26:45 -0300

    (Fix) - LSP lua Keybinds

--- lua/plugins/lsp-configs.lua
+++ lua/plugins/lsp-configs.lua
@@ -15,8 +15,8 @@ return {
       "williamboman/mason-lspconfig.nvim",
       config = function()
         require("mason-lspconfig").setup({
-          ensure_installed = 
-            {"lua_ls", "eslint", "html", "emmet_ls", "cssls", "ast_grep", "jdtls", "jsonls", "harper_ls", "bashls"},
+          ensure_installed = {"lua_ls"}
+
         })
       end
     },
@@ -28,18 +28,9 @@ return {
         local lspconfig = require("lspconfig")
         --initializing the communication between each lsp and the Neovim
         lspconfig.lua_ls.setup({})
-        lspconfig.eslint.setup({})
-        lspconfig.html.setup({})
-        lspconfig.emmet_ls.setup({})
-        lspconfig.cssls.setup({})
-        lspconfig.ast_grep.setup({})
-        lspconfig.jdtls.setup({})
-        lspconfig.jsonls.setup({})
-        lspconfig.harper_ls.setup({})
-        lspconfig.bashls.setup({})
+        vim.keymap.set('n', "K", vim.lsp.buf.hover, {})
       end
     }
 
   }
-
 }
